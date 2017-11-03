cf5feabe0adbffafecd62294b48a0e00e72fa6f4
Elle Faraday <faraday.elle@gmail.com>
2017-11-03 01:09:58 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
-      puts "  #{holiday.capitalize.split("_")}: #{supplies.join(", ")}"
+      puts "  #{holiday.capitalize.sub("_", " ")}: #{supplies.join(", ")}"
     end
   end
 
