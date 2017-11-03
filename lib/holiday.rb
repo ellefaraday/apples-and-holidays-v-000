954dffbbc1826fff3e12a560ca71b0c7fb090936
Elle Faraday <faraday.elle@gmail.com>
2017-11-03 00:46:00 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,6 +39,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
   holiday_hash.each do |season, holidays|
+    binding.pry
     if season == :summer
       holidays.each do |holiday, supplies|
         if holiday == :memorial_day
